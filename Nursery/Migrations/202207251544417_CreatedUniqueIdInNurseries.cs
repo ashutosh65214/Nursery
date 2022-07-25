350da4869321146211dc07a83335ccc5e7c3a137
OMGashu <48097797+ashutosh65214@users.noreply.github.com>
2022-07-25 21:17:08 +0530

    Added Unique column

---
+++ Nursery/Migrations/202207251544417_CreatedUniqueIdInNurseries.cs
@@ -0,0 +1,18 @@
+﻿namespace Nursery.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class CreatedUniqueIdInNurseries : DbMigration
+    {
+        public override void Up()
+        {
+            CreateIndex("dbo.Nurseries", "SKU_NO", unique: true);
+        }
+        
+        public override void Down()
+        {
+            DropIndex("dbo.Nurseries", new[] { "SKU_NO" });
+        }
+    }
+}
--- Nursery/Models/ApplicationDbContext.cs
+++ Nursery/Models/ApplicationDbContext.cs
@@ -15,6 +15,15 @@ namespace Nursery.Models
         public DbSet<Nursery> nurseries { get; set; }
         public DbSet<Category> categories { get; set; }
 
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder); 
+
+            modelBuilder.Entity<Nursery>()
+                .HasIndex(x => x.SKU_NO)
+                .IsUnique();
+        }
+
 
 
 
